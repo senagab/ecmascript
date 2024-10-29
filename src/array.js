945774a4b6b7ff1c2df59fbb8f895b9ff0d23c49
senagab <senagab@gmail.com>
2024-10-29 16:44:32 -0400

    feat: métodos push + every

--- src/array.js
+++ src/array.js
@@ -4,14 +4,14 @@ for (let i = 0; i < redesSociais.length; i++) {
     console.log(`Eu tenho perfil na rede social: ${redesSociais[i]}`);
 }
 
-// es6 forEach
+// forEach
 redesSociais.forEach(function (nomeDaRedeSocial, indice){
     console.log(`#${indice} Rede social: ${nomeDaRedeSocial}`);
 })
 
 const alunos = ['Gustavo', 'Júlia', 'Alice', 'Wagner'];
 
-// es6 map
+// map
 const turma = alunos.map(function (itemAtual) {
     return {
         nome: itemAtual,
@@ -21,14 +21,31 @@ const turma = alunos.map(function (itemAtual) {
 
 console.log(turma);
 
+// find
 const alice = turma.find(function(item) {
     return item.nome == 'Alice'; // resultado booleano: se encontra retorna o item, se não, retorna nada (undefined)
 })
 
 console.log(alice);
 
+
+// find index
 const indiceDaJulia = turma.findIndex(function(item) {
     return item.nome == 'Júlio'; // resultado booleano: se encontra retorna a posição no array, se não, retorna -1
 })
 
 console.log(indiceDaJulia);
+
+// push 
+// adicionar novo aluno ao array
+turma.push({
+    nome: 'Lucio',
+    curso: 'Backend'
+})
+
+// every
+const todosAlunosSaoDeFront = turma.every(function(item) {
+    return item.curso === 'Frontend'; // vai retornar caso o valor do curso seja correspondido 
+})
+
+console.log(todosAlunosSaoDeFront);
