c07f9a9fc95f68d4a6db2647f1d6a9f697f3dc0c
senagab <senagab@gmail.com>
2024-10-29 19:59:49 -0400

    feat rest operator

--- src/rest_spread.js
+++ src/rest_spread.js
@@ -1,3 +1,4 @@
+// arguments
 function somar() {
 
     let soma = 0;
@@ -13,3 +14,14 @@ function somar() {
 }
 
 console.log(somar(10, 2, 3));
+
+// rest
+function somarComRest(...numeros) { // reticencias torna em operador rest
+    const soma = numeros.reduce((total, numeroAtual) => {
+        total += numeroAtual;
+        return total;
+    }, 0)
+    return soma;
+}
+
+console.log(somarComRest(10, 2, 3));
