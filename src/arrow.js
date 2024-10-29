83e2828b4eac2b964053bb610abe95311bd4085c
senagab <senagab@gmail.com>
2024-10-29 19:37:23 -0400

    feat: escopo com arrow function

--- src/arrow.js
+++ src/arrow.js
@@ -22,3 +22,22 @@ const retornaUmCarro = () => ({
 
 console.log(minhaFuncaoReduzida());
 console.log(retornaUmCarro());
+
+const carro = {
+    velocidadeAtual: 40,
+    acelerar: function() { // método acelerar
+        console.log(this);
+        this.velocidadeAtual += 10;
+        console.log("carro acelerando");
+    },
+    frear: () => { // método frear
+        console.log(this);
+        this.velocidadeAtual -= 10;
+        console.log("carro freando");
+    }
+}
+
+carro.acelerar();
+carro.frear();
+
+console.log(carro.velocidadeAtual);
