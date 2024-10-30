d618da6c01bf6a1353d2d5f6ec8a3ff141174203
senagab <senagab@gmail.com>
2024-10-30 10:44:05 -0400

    feat: método spread

--- src/rest_spread.js
+++ src/rest_spread.js
@@ -25,3 +25,18 @@ function somarComRest(...numeros) { // reticencias torna em operador rest
 }
 
 console.log(somarComRest(10, 2, 3));
+
+// spread
+// numeros
+const numeros = [1, 2, 3, 4];
+console.log(...numeros);
+
+// times
+const timesDeFutebolSaoPaulo = ['santos', 'palmeiras', 'bragantino', 'são paulo'];
+const timesDeFutebolRio = ['vasco', 'flamengo', 'botafogo', 'fluminense'];
+
+// concatenação entre arrays
+// const timesDeFutebol = timesDeFutebolSaoPaulo.concat(timesDeFutebolRio);
+const timesDeFutebol = [...timesDeFutebolSaoPaulo, ...timesDeFutebolRio];
+
+console.log(timesDeFutebol);
